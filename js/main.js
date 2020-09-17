6c46160c6487b1be5047d3c6cdeb1480e323eaac
dem585 <65715072+dem585@users.noreply.github.com>
2020-09-17 19:22:58 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(function(){
+	$('.slider__items').slick({
+	prevArrow:'#slider-btn__left',
+	nextArrow:'#slider-btn__right',
+  	infinite: false,
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+  	responsive: [
+            {
+                breakpoint: 910,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+ 
+	});
+
+
+
+	$('.header__menu-m').on('click',function(){
+		$('.menu-pk').toggleClass('menu__pk--active');
+		
+	});
+
+	$('.header__menu-m').on('click',function(){
+		$('.menu-mb').toggleClass('menu__mb--active');
+		
+	});
+
+
+
+	$(window).resize(function(){
+		if($(window).width()<1020){
+			$('.header__menu').removeClass('menu-pk');
+			$(".header__menu").removeClass('menu__pk--active');	
+			$('.header__menu').addClass('menu-mb');	
+		}
+		else{
+			$('.header__menu').addClass('menu-pk');
+		}
+
+		
+	});
+
+	$(window).resize(function(){
+		if($(window).width()>1020){
+			$('.header__menu').removeClass('menu-mb');
+			$(".header__menu").removeClass('menu__mb--active');	
+			
+		}
+		else{
+			$('.header__menu').addClass('menu-mb');
+		}
+
+		
+	});
+
+
+
+  
+});
+
+
+
